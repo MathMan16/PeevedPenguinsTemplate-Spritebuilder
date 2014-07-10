75675ebc614fade19f683b1ce8de199cea66142c
Henry Thaman <hthaman@mit.edu>
2014-07-10 06:33:33 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -177,10 +177,10 @@ static const float MIN_SPEED = 3.f;
 
 - (void)update:(CCTime)delta
 {
-    // if speed is below minimum speed, assume this attempt is over
+    /*// if speed is below minimum speed, assume this attempt is over
     if(_currentPenguin == nil){
         return;
-    }
+    }*/
     if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED){
         //NSLog(@"Low Speed");
         [self nextAttempt];
