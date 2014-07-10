1dea86d269e8a117d0a32fce547544de2b5bd6dc
Henry Thaman <hthaman@mit.edu>
2014-07-10 05:30:25 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -199,5 +199,8 @@ static const float MIN_SPEED = 10.f;
     CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0, 0)];
     [_contentNode runAction:actionMoveTo];
     NSLog(@"Stopped Following");
+    if (_currentPenguin == nil){
+        NSLog(@"No Penguin");
+    }
 }
 @end
